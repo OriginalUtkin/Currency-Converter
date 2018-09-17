2f69bcc77c61682115d34b78a5e5660cd16cc6a0
Kirill Utkin <utkinkir@gmail.com>
2018-09-17 20:40:53 +0200

    Final version of arguments parser(without validating)

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import argparse
 
 
-def parse_arguments():
-
-    # TODO: Descriptions for parser, arguments and help
+def parse_args():
 
     """
-
-    :return:
+    Configure argparse object for working with input arguments
+    :return: dictionary which has a following format -> input_argument_name: argument_value
     """
 
-    parser = argparse.ArgumentParser("First version of argument parser")
+    parser = argparse.ArgumentParser(description="Process input arguments for currency converter")
 
-    parser.add_argument('--amount')
-    parser.add_argument('--input_currency')
-    parser.add_argument('--output_currency')
+    parser.add_argument('--amount', help="Converting amount. Should be a number value", default=1, type=float)
+
+    parser.add_argument('--input_currency', help="Input currency for converting.Should be represented by 3 letters "
+                                                 "name or currency symbol", required=True)
+
+    parser.add_argument('--output_currency', help="Output currency. 3 letters or currency symbol."
+                                                  "If this parameter is missing program will convert "
+                                                  "to all known currencies.")
 
     return vars(parser.parse_args())
 
 
 if __name__ == '__main__':
-    parsed_arguments = parse_arguments()
+    parsed_arguments = parse_args()
