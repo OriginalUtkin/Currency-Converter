f653e6ae89f744648ee9b38f4d85fa31db5b1775
Kirill Utkin <utkinkir@gmail.com>
2018-09-17 19:19:17 +0200

    allowable arguments for parser

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ import argparse
 
 
 def parse_arguments():
+
+    # TODO: Descriptions for parser, arguments and help
+
     """
 
     :return:
     """
-    pass
+
+    parser = argparse.ArgumentParser("First version of argument parser")
+
+    parser.add_argument('--amount')
+    parser.add_argument('--input_currency')
+    parser.add_argument('--output_currency')
+
+    return vars(parser.parse_args())
 
 
 if __name__ == '__main__':
-    parse_arguments()
+    parsed_arguments = parse_arguments()
