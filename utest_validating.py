eda1e57ecfdf1e0e926ee66cf5fdd47b4f4546f6
Kirill Utkin <utkinkir@gmail.com>
2018-09-18 15:02:46 +0200

    New tests was added

--- utest_validating.py
+++ utest_validating.py
@@ -1,28 +1,38 @@
 import unittest
+import argparse
 import main as currency_converter
 
-# TODO: Check negative value
-# TODO: Such a big value, wow
-# TODO: Arguments are passed to py script as strings -> test them for correct format for float func
 
+class ValidatingAmountTest(unittest.TestCase):
 
-class CurrencyConverterTest(unittest.TestCase):
+    def test_value(self):
+        self.assertEqual(currency_converter.validate_amount(10), 10.0)
+        self.assertEqual(currency_converter.validate_amount(2.11), 2.11)
 
     def test_negative_value(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(argparse.ArgumentTypeError):
             currency_converter.validate_amount(-34.14)
 
-    # def test_value(self):
-    #     self.assertEqual(currency_converter.validate_amount(10), 10.0)
-    #     self.assertEqual(currency_converter.validate_amount(2.11), 2.11)
-    #
-    # def test_instance_of(self):
-    #     self.assertTrue(isinstance(currency_converter.validate_amount(10)), float)
-    #     self.assertTrue(isinstance(currency_converter.validate_amount(2.11)), float)
-    #
-    # def test_string_value(self):
-    #     self.assertRaises(ValueError, currency_converter.validate_amount(" "))
-    #     # self.assertRaises(ValueError, currency_converter.validate_amount("22.0q"))
+    def test_NaN(self):
+        with self.assertRaises(argparse.ArgumentTypeError):
+            currency_converter.validate_amount(float('NaN'))
+
+    def test_Inf(self):
+        with self.assertRaises(argparse.ArgumentTypeError):
+            currency_converter.validate_amount(float('Inf'))
+
+    def test_string_value(self):
+        with self.assertRaises(ValueError):
+            currency_converter.validate_amount("22.0qw")
+
+    @unittest.skip
+    def test_empty_string(self):
+        with self.assertRaises(ValueError):
+            currency_converter.validate_amount(" ")
+
+    def test_instance_of(self):
+        self.assertTrue(isinstance(currency_converter.validate_amount(10), float))
+        self.assertTrue(isinstance(currency_converter.validate_amount(2.11), float))
 
 
 if __name__ == '__main__':
