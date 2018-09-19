70270800a94cd8b09090fcfc28766c621b887d0b
Kirill Utkin <utkinkir@gmail.com>
2018-09-19 12:14:59 +0200

    new tests was added

--- utest_currency.py
+++ utest_currency.py
@@ -6,12 +6,23 @@ from main import validate_currency
 class ValidatingCurrencyTest(unittest.TestCase):
 
     def test_return(self):
-        self.assertEqual("CzK", "CZK")
+        self.assertEqual(validate_currency("CzK"), "CZK")
 
     def test_length(self):
-        with self.assertRaises(argparse.ArgumentError):
+        with self.assertRaises(argparse.ArgumentTypeError):
             validate_currency("CZKU")
 
     def test_not_existing_currency(self):
-        with self.assertRaises(argparse.ArgumentError):
+        with self.assertRaises(argparse.ArgumentTypeError):
             validate_currency("LOL")
+
+    def test_symbol_doesnt_exist(self):
+        with self.assertRaises(argparse.ArgumentTypeError):
+            validate_currency("%")
+
+    def test_symbol_exists(self):
+        self.assertEqual("$", "$")
+
+    @unittest.skip("Function can't do that right now")
+    def convert_symbol_to_currency(self):
+        pass
