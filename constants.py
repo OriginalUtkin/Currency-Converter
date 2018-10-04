83b3b23f14a1377a1da550687809378aa259a9a0
Kirill Utkin <utkinkir@gmail.com>
2018-10-04 23:08:33 +0200

    Added format string for logger

--- constants.py
+++ constants.py
@@ -2,3 +2,4 @@ often_used_currencies = ["EUR", "USD", "RUB", "CZK", "GBP", "AUD", "JPY", "CHF"]
 currencies_list_addr = "https://free.currencyconverterapi.com/api/v6/currencies"
 converting_request = "https://free.currencyconverterapi.com/api/v6/convert?q={0}_{1}&compact=y"
 expire_time = 43200
+logger_format="%(asctime)s:%(levelname)s:%(module)s:%(message)s"
